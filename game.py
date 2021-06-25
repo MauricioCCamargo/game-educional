ae1ca2a20c56af15ef721ee0ce2f628d4bef83ae
mauricio camargo <mauricio8418@gmail.com>
2021-06-25 00:09:51 -0300

    versão final

--- game.py
+++ game.py
@@ -3,12 +3,19 @@ import os
 import random
 from funcoes import colisaoparede
 import time
+import sys
+nome = input("insira seu nome:")
+email= input("insira seu e-mail:")
+sys.stdout = open("log.txt", "w")
+print(nome, email)
+sys.stdout.close
 pygame.init()
 fundo = pygame.image.load("assets/fundo.png")
 largura = 1026
 altura = 728
 display = pygame.display.set_mode((largura, altura))
 display.blit(fundo,(0,0))
+pygame.display.set_caption("Jogo da Reciclagem")
 fps = pygame.time.Clock()
 lixovermelho = pygame.image.load("assets/Vermelho_.png")
 lixoazul = pygame.image.load("assets/Azul_.png")
@@ -34,21 +41,22 @@ def message_display(text):
 message_display("voce devera acertar os residuos sólidos em seus devidos lugares")
 display.blit(fundo,(0,0))
 message_display("mova-os para os lados utilizando as setas")
-#texto1 = letras.render("voce devera acertar os residuos sólidos em seus devidos lugares",True,cor1)
-
-#texto2 = letras.render("mova-os para os lados utilizando as setas",True,cor1)
 
 def escrevendoPlacar(acertos):
     font = pygame.font.SysFont(None, 25)
     texto = font.render("Acertos:"+str(acertos), True, cor1)
     display.blit(texto, (0, 0))
+def escrevendoPlacarE(erros):
+    font = pygame.font.SysFont(None, 25)
+    texto = font.render("Erros:"+str(erros), True, cor1)
+    display.blit(texto, (940, 0))
 def jogo():
     movimentoX = 0
     lixoPosicaoX = largura * 0.45
     lixoPosicaoY = 0 
-    lixoVelocidade = 3
-    LarguraPapel = 114
-    alturaPapel = 109
+    lixoVelocidade = 4.5
+    LarguraPapel = 114 -50
+    alturaPapel = 109-50
     azulposicaoX = 750
     azulposicaoY = 500
     amareloposicaoX = 50
@@ -57,29 +65,34 @@ def jogo():
     verdeposicaoY = 500
     vermelhoposicaoX = 530
     vermelhoposicaoY = 500
-    larguraverde = 175
-    alturaverde = 106
-    larguravermelho = 178
-    alturavermelho = 106
-    larguraazul = 181
-    alturaazul =108
-    larguraamarelo = 175
-    alturaamarelo = 105
-    larguraPlastico = 100
-    alturaPlastico = 208
-    larguraMetal  = 100
-    alturaMetal = 174
-    larguraVidro = 77
-    alturaVidro = 149
+    larguraverde = 175 -50
+    alturaverde = 106-50
+    larguravermelho = 178-50
+    alturavermelho = 106-50
+    larguraazul = 181 -50
+    alturaazul =108-50
+    larguraamarelo = 175-50
+    alturaamarelo = 105-50
+    larguraPlastico = 100-50
+    alturaPlastico = 208-50
+    larguraMetal  = 100-50
+    alturaMetal = 174-50
+    larguraVidro = 77-50
+    alturaVidro = 149-50
     acertos = 0
+    erros = 0
     pygame.mixer.music.load('assets/musica.wav')
     pygame.mixer.music.play(-1)
     lixoescolhido = random.choice(lixos)
+    
+    
     while True:
         
         
-        escrevendoPlacar(acertos)
+        
         display.blit(fundo,(0,0))
+        escrevendoPlacar(acertos)
+        escrevendoPlacarE(erros)
         display.blit(lixoamarelo,(amareloposicaoX,amareloposicaoY ))
         display.blit(lixoverde,(verdeposicaoX,verdeposicaoY ))
         display.blit(lixovermelho,(vermelhoposicaoX,vermelhoposicaoY))
@@ -109,40 +122,54 @@ def jogo():
         if lixoPosicaoY  > 728 - alturaPapel:
             lixoPosicaoY = 0 
             lixoescolhido = random.choice(lixos)
-            message_display("você deve acertar a lixeira correta")
-            time.sleep(2)
+            message_display("você deve acertar a lixeira correta!")
+            time.sleep(1)
+            erros += 1
+            
         if lixoescolhido == plastico:
-            if lixoPosicaoY + alturaPlastico >= vermelhoposicaoY and lixoPosicaoX >= vermelhoposicaoX and lixoPosicaoX <= vermelhoposicaoX + larguravermelho: 
-                message_display("você acertou")
-                time.sleep(2)
+            if lixoPosicaoY + alturaPlastico >= vermelhoposicaoY and lixoPosicaoY <= vermelhoposicaoY + alturavermelho and lixoPosicaoX >= vermelhoposicaoX and lixoPosicaoX <= vermelhoposicaoX + larguravermelho: 
+                message_display("você acertou!")
+                time.sleep(1)
                 lixoPosicaoX = largura * 0.45
                 lixoPosicaoY = 0
                 lixoescolhido = random.choice(lixos)
-                acertos =+ 1
+                acertos += 1
+                
         if lixoescolhido == metal:
-            if lixoPosicaoY + alturaMetal >= amareloposicaoY and lixoPosicaoX >= amareloposicaoX and lixoPosicaoX <= amareloposicaoX + larguraamarelo: 
-                message_display("você acertou")
-                time.sleep(2)
+            if lixoPosicaoY + alturaMetal >= amareloposicaoY and lixoPosicaoY <= amareloposicaoY + alturaamarelo and lixoPosicaoX >= amareloposicaoX and lixoPosicaoX <= amareloposicaoX + larguraamarelo: 
+                message_display("você acertou!")
+                time.sleep(1)
                 lixoPosicaoX = largura * 0.45
                 lixoPosicaoY = 0
                 lixoescolhido = random.choice(lixos)
-                acertos=+ 1
+                acertos+= 1
+                
         if lixoescolhido == vidro:
-            if lixoPosicaoY + alturaVidro >= verdeposicaoY and lixoPosicaoX >= verdeposicaoX and lixoPosicaoX <= verdeposicaoX + larguraverde: 
-                message_display("você acertou")
-                time.sleep(2)
+            if lixoPosicaoY + alturaVidro >= verdeposicaoY and lixoPosicaoY <= verdeposicaoY + alturaverde and lixoPosicaoX >= verdeposicaoX and lixoPosicaoX <= verdeposicaoX + larguraverde: 
+                message_display("você acertou!")
+                time.sleep(1)
                 lixoPosicaoX = largura * 0.45
                 lixoPosicaoY = 0
                 lixoescolhido = random.choice(lixos)
-                acertos=+ 1
+                acertos+= 1
+                
         if lixoescolhido == papel:
-            if lixoPosicaoY + alturaPapel >= azulposicaoY and lixoPosicaoX >= azulposicaoX and lixoPosicaoX <= azulposicaoX + larguraazul: 
-                message_display("você acertou")
-                time.sleep(2)
+            if lixoPosicaoY + alturaPapel >= azulposicaoY and lixoPosicaoY <= azulposicaoY + alturaazul and lixoPosicaoX >= azulposicaoX and lixoPosicaoX <= azulposicaoX + larguraazul: 
+                message_display("você acertou!")
+                time.sleep(1)
                 lixoPosicaoX = largura * 0.45
                 lixoPosicaoY = 0
                 lixoescolhido = random.choice(lixos)
-                acertos =+ 1
+                acertos += 1
+                
+        if acertos >= 10:
+            display.blit(fundo,(0,0))
+            message_display("você venceu, parabéns!")
+            break
+        if erros > 10:
+            display.blit(fundo,(0,0))
+            message_display("você perdeu :( tente novamente")
+            break
         pygame.display.update()
         fps.tick(60)
 jogo()     
